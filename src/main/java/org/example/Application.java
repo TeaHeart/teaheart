8cc8ce565279f2f0a8f1d3874979efeb348f6f66
TeaHeart <1069696634@qq.com>
2025-02-21 13:46:56 +0800

    feat: spring-template

---
+++ src/main/java/org/example/Application.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Slf4j
+@EnableScheduling
+@SpringBootApplication
+@MapperScan("org.example.mapper")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
