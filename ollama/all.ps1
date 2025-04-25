0ee697314e32a843e66e6f62b220beed06278e97
TeaHeart <1069696634@qq.com>
2025-04-25 22:43:54 +0800

    feat: 批量创建modelfile脚本

--- ollama/all.ps1
+++ ollama/all.ps1
@@ -1,3 +1,4 @@
+# 拉取模型
 (
     'qwen2.5:7b',
     'qwen2.5-coder:1.5b',
@@ -15,10 +16,39 @@
     ollama pull $_
 }
 
-# ls -File def | % {
-#     ollama create "teaheart/${$_.Name.Replace("@", ":")}" -f $_.FullName
-# }
+# 删除自定义模型
+ollama list | % {
+    $name = ($_ -split "\s+", 2)[0]
+    if ($name -like "teaheart*") {
+        ollama rm $name
+    }
+}
 
-ollama list
+# 创建modelfile文件并写入一些参数
+ollama list | % {
+    $name = ($_ -split "\s+",2)[0]
+    
+    if ($name -like "NAME" -or $name -like "teaheart*") {
+        return
+    }
 
-Create-OllamaModelsLink
+    $sb = [System.Text.StringBuilder]::new()
+    $sb.AppendLine("FROM {0}") > $null
+    $sb.AppendLine() > $null
+    $sb.AppendLine("PARAMETER num_gpu 32") > $null
+    $sb.AppendLine("PARAMETER num_batch 32") > $null
+    $sb.Append("{1}") > $null
+    $sb = $sb.ToString()
+
+    $pureName = ($name -split "/",2)[-1]
+    $temp = $name -like "*coder*" ? "PARAMETER temperature 0" : ""
+    $filename = $pureName.Replace(":", "@")
+    $modelfile = $sb -f $name, $temp
+    $modelfile > "./def/$filename"
+}
+
+# 根据文件创建模型
+ls -File def | % {
+    $name = "teaheart/" + $_.Name.Replace("@", ":")
+    ollama create $name -f $_.FullName
+}
