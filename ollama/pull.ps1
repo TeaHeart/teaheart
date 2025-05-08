281a8287612bd49fdcbaf45705956bd3ae7cb6e6
TeaHeart <1069696634@qq.com>
2025-05-08 13:33:14 +0800

    chore: 更新

--- ollama/pull.ps1
+++ ollama/pull.ps1
@@ -1,14 +1,15 @@
 # 拉取模型
 (
+    'qwen2.5-coder:1.5b',
+    'qwen3:0.6b',
     'qwen3:1.7b',
     'qwen3:4b',
     'qwen3:8b',
     'qwen3:30b',
     'huihui_ai/qwen2.5-coder-abliterate:1.5b',
+    'huihui_ai/qwen3-abliterated:0.6b',
     'huihui_ai/qwen3-abliterated:1.7b',
-    # 'huihui_ai/qwen3-abliterated:4b',
-    # 'huihui_ai/qwen3-abliterated:8b',
-    # 'huihui_ai/qwen3-abliterated:30b',
+    'huihui_ai/qwen3-abliterated:4b',
     'qllama/bce-embedding-base_v1:q4_k_m',
     'qllama/bce-reranker-base_v1:q4_k_m'
 ) | % {
